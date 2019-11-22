ae23b6938d44490b887ad2aa91b8f0711e6dd949
thomp <thomp@protonmail.com>
2019-11-22 07:35:44 -0600

    finished Arrays

--- scrap.rb
+++ scrap.rb
@@ -1,2 +1,13 @@
-some_symbols = "hello".methods.grep /case/
-p some_symbols
+a = [1, 3, 4, 7, 8, 10]
+a.each { |num| print num }
+puts
+
+new_arr = a.select { |num| num > 4 }
+p new_arr
+new_arr = a.select { |num| num < 10 }
+           .reject{ |num| num.even? }
+p new_arr
+
+# Multiply each element by 3 producing new array
+new_arr = a.map {|x| x * 3}
+p new_arr
