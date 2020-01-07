9a1c381b4c05799ac02d721b7a31c49f0b7b7c64
thomp <thomp@protonmail.com>
2020-01-07 08:12:06 -0600

    completed Modules

--- scrap.rb
+++ scrap.rb
@@ -1,18 +1,14 @@
-class Dog
-  def to_s
-    "Dog"
-  end
-  def bark
-    "barks loudly"
-  end
-end
-class SmallDog < Dog
-  def bark
-    "barks quietly"
-  end
-end
+require_relative 'player'
+require_relative 'team'
 
-dog = Dog.new # (btw, new is a class method)
-small_dog = SmallDog.new
-puts "#{dog}1 #{dog.bark}"
-puts "#{small_dog}2 #{small_dog.bark}"
+player1 = Player.new("Bob", 13, 5); player2 = Player.new("Jim", 15, 4.5)
+player3 = Player.new("Mike", 21, 5); player4 = Player.new("Joe", 14, 5)
+player5 = Player.new("Scott", 16, 3)
+
+red_team = Team.new("Red")
+red_team.add_players(player1, player2, player3, player4, player5) # (splat)
+
+# select only players between 14 and 20 and reject any player below 4.5 skill-level
+elig_players = red_team.select {|player| (14..20) === player.age}.reject{|player| player.skill_level < 4.5}
+
+puts elig_players
