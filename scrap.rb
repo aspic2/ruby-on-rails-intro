1f6a1d63b140a12b3b90b830f9b742711b14284f
thomp <thomp@protonmail.com>
2020-03-22 11:58:17 -0500

    finished Controller and View

--- scrap.rb
+++ scrap.rb
@@ -1,16 +1,1 @@
-# calculator_spec.rb
-
-require 'rspec'
-require_relative '../calculator'
-
-describe Calculator do
-  before { @calculator = Calculator.new('RSpec calculator')}
-
-  it "should add 2 numbers correctly" do
-    expect(@calculator.add(2, 2)).to eq 4
-  end
-
-  it "should subtract 2 numbers correctly" do
-    expect(@calculator.subtract(4, 2)).to eq 2
-  end
-end
+rails g controller greeter hello
