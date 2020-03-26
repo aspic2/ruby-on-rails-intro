31efb95de647c9e574f7dcf7ad6403745c7ace90
thomp <thomp@protonmail.com>
2020-03-26 08:12:19 -0500

    finished HTTParty

--- scrap.rb
+++ scrap.rb
@@ -1,1 +1,19 @@
-rails g controller greeter hello
+require 'httparty'
+require 'pp'
+
+class Coursera
+  include HTTParty
+
+  base_uri 'https://api.coursera.org/api/catalog.v1/courses'
+  # these are params you want to include in every request
+  default_params fields: "smallIcon,shortDescription", q: "search"
+  format :json
+
+  # you can have multiple methods. The settings above apply for all methods
+  # in the class
+  def self.for term
+    get("", query: {query: term})["elements"]
+  end
+end
+
+pp Coursera.for "python"
